2d6b0ecb7876f2643941841db0d4d94112471f2d
CHEYOUNG-LIM <im7728@naver.com>
2023-05-28 15:46:15 +0900

    "Refactor"

--- server/wrieating/src/main/java/com/mainproject/wrieating/dataArchive/dbsource/DatabaseConnection.java
+++ server/wrieating/src/main/java/com/mainproject/wrieating/dataArchive/dbsource/DatabaseConnection.java
@@ -8,7 +8,9 @@ public class DatabaseConnection {
     private static final String DB_URL = "";
     private static final String DB_USER = "";
     private static final String DB_PASSWORD = "";
-
+//    private static final String DB_URL = "jdbc:h2:mem:test";
+//    private static final String DB_USER = "sa";
+//    private static final String DB_PASSWORD = "";
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
     }
