429dd3ef86a6ec78bef09f6363069b9e0d6c73dd
CHEYOUNG-LIM <im7728@naver.com>
2023-05-29 17:07:54 +0900

    "refactor"

--- server/wrieating/scripts/server_start.sh
+++ server/wrieating/scripts/server_start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/build
-sudo nohup java -jar wrieating-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/action/deploy.log
+sudo JWT_SECRET_KEY=$JWT_SECRET_KEY AWS_RDS_PASSWORD=$AWS_RDS_PASSWORD AWS_RDS_URL=$AWS_RDS_URL profile=$profile email=$email password=$password nohup java -jar -Dspring.profiles.active=deploy wrieating-0.0.1-SNAPSHOT.jar >> /home/ec2-user/action/deploy.log 2>/home/ec2-user/action/deploy_err.log &
